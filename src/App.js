111cf02afd1212b8857d2bb009ad60af9a3b194f
MalMWare <malkelleymartin@gmail.com>
2023-03-19 10:02:43 -0700

    added react-loader-spinner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react';
 import './App.css';
+import { Bars } from 'react-loader-spinner'
 const AboutUs = React.lazy(() => import('./components/AboutUs'))
 const Packages = React.lazy(() => import('./components/Packages'))
+
 
 function App() {
   return (
@@ -9,7 +11,11 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-        <Suspense fallback={<h1>Loading...</h1>}>
+        <Suspense fallback={<Bars
+            color="FF00FF"
+            height={200}
+            width={200}
+            style={{margin: "0 auto"}}/>}>
           <AboutUs />
           <Packages />
         </Suspense>
